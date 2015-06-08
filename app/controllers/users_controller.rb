b25ed98e6b0471c91594394a86f704bfa1494591
Dan A <dadeyan1@bighamton.edu>
2015-06-07 21:55:49 -0400

    add micropost model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.all # retrieve list of all users from db
   end
 
   # GET /users/1
